8f818d3af201a3ace9560c509e2e6543d0261a45
James Robinson <github@epii.co.uk>
2014-10-05 15:32:35 +0100

    Made login redirect just go to google auth

--- site.js
+++ site.js
@@ -119,5 +119,5 @@ db.connect(function() {
 
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login')
+  res.redirect('/auth/google')
 }
