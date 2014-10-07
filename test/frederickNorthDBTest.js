e4dd8e1bd1bb3f407161ec77325a80121acf4726
James Robinson <github@epii.co.uk>
2014-10-07 08:22:20 +0100

    Wrote a test to check the get leaflets method works

---
+++ test/frederickNorthDBTest.js
@@ -0,0 +1,16 @@
+var expect = require("chai").expect
+var db = require("../lib/frederickNorthDB");
+describe("frederickNorthDB", function() {
+  describe("#getLeaflets", function() {
+    it('should return precisely one leaflet entry from the test database', function(done) {
+      db.connect(function() {
+        db.getLeaflets(100274569354905081446, function(err, leaflets) {
+          expect(err).to.equal(null);
+          expect(leaflets.length).to.equal(1);
+          expect(leaflets[0]).to.equal("78bb9ca9-1c5c-474d-b02e-14d698eef0f7");
+          done();
+        });
+      });
+    });
+  });
+});
