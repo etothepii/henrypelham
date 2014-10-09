9dfd28dca2440ee918924f8116fcbed1d9c84ba2
James Robinson <github@epii.co.uk>
2014-10-09 09:14:49 +0100

    Improved Tree to use bootstrap glyphs for expand and contract and added a little padding and margin to make line up with the nodes

--- public/javascripts/treeHidder.js
+++ public/javascripts/treeHidder.js
@@ -1,10 +1,16 @@
   function contractTree(id) {
     $("#a" + id).attr("onclick","expandTree('" + id + "')");
-    $("#a" + id).text("+");
+    $("#span" + id).addClass("glyphicon-plus");
+    $("#span" + id).removeClass("glyphicon-minus");
+    $("#span" + id).removeClass("treeContractor");
+    $("#span" + id).addClass("treeExpander");
     $("#ul" + id).addClass("hidden");
   }
   function expandTree(id) {
     $("#a" + id).attr("onclick","contractTree('" + id + "')");
-    $("#a" + id).text("-");
+    $("#span" + id).removeClass("glyphicon-plus");
+    $("#span" + id).addClass("glyphicon-minus");
+    $("#span" + id).addClass("treeContractor");
+    $("#span" + id).removeClass("treeExpander");
     $("#ul" + id).removeClass("hidden");
   }
