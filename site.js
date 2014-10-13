9bec6c1e26bdd8edb47f579f81032287181b2ec2
James Robinson <github@epii.co.uk>
2014-10-14 00:13:19 +0100

    When opening up the leafletMap by scanning the QR code it automatically marks the leafletMap as delivered

--- site.js
+++ site.js
@@ -124,7 +124,7 @@ app.get('/purchase', function(req, res){
 });
 
 app.get('/pl/:leafletMapId', function (req, res) {
-  db.getLeafletMap(req.params.leafletMapId, function(err, leafletMap) {
+  db.getLeafletMapAndMarkAsDelivered(req.params.leafletMapId, function(err, leafletMap) {
     if (err) {
       res.render('error', {err: err});
     }
